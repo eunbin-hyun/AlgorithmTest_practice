ccb4b729df4fa95008bd3a7ffffbd097e2c0b706
eunbin-hyun <eunbin8592@gmail.com>
2025-03-03 21:47:17 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.24 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,7 +1,6 @@
 def solution(my_string, queries):
     answer = my_string
     for s,e in queries:
-        ans1 = answer[s:e+1]
-        ans2 = ''.join(reversed(ans1))
-        answer = ''.join([answer[:s], ans2, answer[e+1:]])
+        ans = answer[s:e+1][::-1]
+        answer = answer[:s] + ans + answer[e+1:]
     return answer
