61d6df95430122063bb26661ec818b2804634ff9
eunbin-hyun <eunbin8592@gmail.com>
2025-03-04 15:03:01 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,9 +1,3 @@
 def solution(hp):
-    answer = 0
-    i = hp // 5 
-    hp = hp % 5
-    j = hp // 3
-    hp = hp % 3
-    k = hp // 1
-    answer = i + j + k
+    answer = hp // 5  + (hp % 5) // 3 + (hp % 5) %3
     return answer
