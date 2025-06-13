65e033f6f209b99e1800c59b36f635f24c655212
eunbin-hyun <eunbin8592@gmail.com>
2025-06-13 14:19:01 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,26 +1,9 @@
-s = input()
+word = input()
 result = 0
+dial = ['1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ', '0']
 
-for i in s:
-    if i == '1':
-        result += 2
-    elif i in 'ABC':
-        result += 3
-    elif i in 'DEF':
-        result += 4
-    elif i in 'GHI':
-        result += 5
-    elif i in 'JKL':
-        result += 6
-    elif i in 'MNO':
-        result += 7
-    elif i in 'PQRS':
-        result += 8
-    elif i in 'TUV':
-        result += 9
-    elif i in 'WXYZ':
-        result += 10
-    elif i in '0':
-        result += 11
-    
+for i in range(len(word)):
+    for j in dial:
+        if word[i] in j:
+            result += (dial.index(j)+2)
 print(result)
