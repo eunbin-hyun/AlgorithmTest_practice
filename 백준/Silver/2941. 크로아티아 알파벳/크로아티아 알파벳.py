323c3b1eee02aaf76b974bd9b2c97ca32e20873e
eunbin-hyun <eunbin8592@gmail.com>
2025-06-19 11:44:02 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,26 +1,8 @@
 s= input()
 alp= ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-n= 0
-count = 0
-i=1
-
-while n < len(s):
-    if n+i <= len(s):
-        if s[n:n+i] in alp:
-            count += 1 
-            n = n+i 
-            i = 1 
-            continue
-        if i >= 3:
-            n += 1
-            i = 1 
-            count += 1 
-            continue
-        i += 1
-    else:
-        count += (len(s)- n)
-        n = len(s)
+for a in alp:
+    s = s.replace(a,'*')
         
-print(count)
+print(len(s))
     
