a2e62879916855f6ec62e5c5eb47c94d58290192
eunbin-hyun <eunbin8592@gmail.com>
2025-07-07 10:43:59 +0900

    [Bronze III] Title: 대지, Time: 84 ms, Memory: 35916 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,9 +1,11 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 x_list =[]
 y_list =[]
 
 for i in range(n):
-    x, y = map(int, input().split())
+    x, y = map(int, sys.stdin.readline().split())
     x_list.append(x)
     y_list.append(y)
     
