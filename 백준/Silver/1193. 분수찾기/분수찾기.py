7ee493662a19b59fc204c98c01ddf216d81bbe52
eunbin-hyun <eunbin8592@gmail.com>
2025-07-07 13:22:07 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -4,21 +4,13 @@ ans = int(sys.stdin.readline())
 
 for i in range(1,ans+1):
     total = (i*(i+1))//2
-    if total == ans:
+    if total >= ans:
         n = i
-        s = 0
-        break
-    elif total > ans:
-        n = i
-        s = (n*(n+1))//2 -ans
+        s = (n*(n+1))//2 - ans
         break
 
-
-if ans == 1:
-    print(f"1/1")
-elif n % 2 == 0:
-    seg = n-s
-    print(f"{seg}/{n-seg+1}")
+if n % 2 == 0:
+    print(f"{n - s}/{s + 1}")
 else:
     seg = n-s 
-    print(f"{n-seg+1}/{seg}")
+    print(f"{s + 1}/{n - s}")
