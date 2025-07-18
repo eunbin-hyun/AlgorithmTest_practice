4d5dd4583e95f0fc87fc6ddb5a136dfe82662470
eunbin-hyun <eunbin8592@gmail.com>
2025-07-18 22:26:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,31 +1,12 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
+result =0
 
-cnt = n//5 + 1
-k = cnt*5
-result = 0
-
-while True:
-    if k == n:
-        result = cnt
+while n >=0:
+    if n % 5 == 0:
+        result += n//5
+        print(result)
         break
-    elif n == 3 or n==5:
-        print(1)
-        break
-    elif n == 1 or n ==2 or n==4:
-        print(-1)
-        break
-    elif k == cnt*3 and n%3 != 0:
-        print(-1)
-        break
-    elif k > n:
-        k -= 5
-        cnt -= 1
-    elif k < n :
-        k += 3
-        cnt += 1
-
-if result !=0:
-    print(result)
+    n -=3
+    result += 1
+else:
+    print(-1)
