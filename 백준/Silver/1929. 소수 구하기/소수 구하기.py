cfc29d27a68efce648db936c7a0b81b1489ade23
eunbin-hyun <eunbin8592@gmail.com>
2025-07-18 14:58:31 +0900

    [Silver III] Title: 소수 구하기, Time: 632 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,18 +1,15 @@
-import math 
 m,n = map(int, input().split())
 array = [True for i in range(n+1)]
+array[1] = 0
 
     
-for i in range(2, int(math.sqrt(n))+1):
+for i in range(2, int(n**(0.5)) +1):
     if array[i] == True: # i가 소수인 경우
         j = 2
         while i * j <= n:
             array[i*j] = False
             j += 1 
-if m < 2:
-    m = 2
     
 for i in range(m, n+1):
     if array[i]:
         print(i)
-    
