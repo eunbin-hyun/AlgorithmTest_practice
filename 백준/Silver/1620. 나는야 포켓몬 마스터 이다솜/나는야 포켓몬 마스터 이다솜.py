6ca0e94a5f68adc3397055648377a74c006d3c4f
eunbin-hyun <eunbin8592@gmail.com>
2025-07-25 17:28:54 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 220 ms, Memory: 55116 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,20 +1,18 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 m, n = map(int, input().split())
-name_index = {}
-dp = deque()
+name_to_num = {}
+num_to_name = {}
 
 for i in range(m):
     name = input().strip()
-    dp.append(name)
-    name_index[name] = i+1
-    
+    name_to_num[name] = i+1
+    num_to_name[i+1] = name
 
 for _ in range(n):
     k = input().strip()
     if k.isdigit():
-        print(dp[int(k)-1])
+        print(num_to_name[int(k)])
     else:
-        print(name_index[k])
+        print(name_to_num[k])
