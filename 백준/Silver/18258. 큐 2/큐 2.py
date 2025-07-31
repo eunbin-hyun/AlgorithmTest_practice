347a46df9057ae39aad7b85b36bd6d5ce78ad887
eunbin-hyun <eunbin8592@gmail.com>
2025-07-31 15:55:19 +0900

    [Silver IV] Title: 큐 2, Time: 1764 ms, Memory: 144224 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -2,39 +2,36 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-k = []
 q = deque()
 n = int(input())
 
-for _ in range(n):
-    kk = list(input().split())
-    k.append(kk)
-
 for i in range(n):
-    if len(k[i]) == 2:
-        q.append(k[i][1])
-    elif k[i][0] == 'pop':
+    k = list(input().split())
+
+    if len(k) == 2:
+        q.append(k[1])
+    elif k[0] == 'pop':
         if not q:
             print(-1)
         else:
             j = q.popleft()
             print(j)
-    elif k[i][0] =='size':
+    elif k[0] =='size':
         if q:
             print(len(q))
         else:
             print(0)
-    elif k[i][0] == 'empty':
+    elif k[0] == 'empty':
         if q:
             print(0)
         else:
             print(1)
-    elif k[i][0] == 'front':
+    elif k[0] == 'front':
         if q:
             print(q[0])
         else:
             print(-1)
-    elif k[i][0] == 'back':
+    elif k[0] == 'back':
         if q:
             print(q[-1])
         else:
