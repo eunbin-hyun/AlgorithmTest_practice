fbdd1a2c1defcdc6185961f1680662d31d65b347
eunbin-hyun <eunbin8592@gmail.com>
2025-08-08 17:00:51 +0900

    [Silver IV] Title: 가로수, Time: 112 ms, Memory: 44056 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,30 +1,21 @@
-import sys
+import sys, math
 input = sys.stdin.readline
 
 n = int(input())
 tree = []
-tree_temp = []
+diff = []
 
 for _ in range(n):
     tree.append(int(input()))
 
 for i in range(1,n):
-    tree_temp.append(tree[i]- tree[i-1])
+    diff.append(tree[i]- tree[i-1])
 
-tree_tp = list(set(tree_temp))
-result = 1000000000
-for i in range(len(tree_tp)):
-    a = tree_tp[i]
-    k = min(tree_temp)
-    while k != 0:
-        temp = a % k
-        a = k
-        k = temp
-    result = min(a, result)
+g = diff[0]
+for d in diff[1:]:
+    g = math.gcd(g, d)
 
 cnt = 0
-for i in range(len(tree_temp)):
-    if tree_temp[i] > result:
-        cnt += tree_temp[i] // result -1
-    
+for d in diff:
+    cnt += d // g -1
 print(cnt)
