9f13639fa7255e660152e5376de421a5749791c6
eunbin-hyun <eunbin8592@gmail.com>
2025-08-22 17:07:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 476 ms, Memory: 134980 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+imput = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n+1):
+    graph[i].sort(reverse = True)
+
+queue = deque([r])
+visited = [0] *(n+1)
+cnt = 0
+
+while queue:
+    k = queue.popleft()
+
+    if visited[k]:
+        continue
+    cnt += 1
+    visited[k] = cnt
+
+    if graph[k]:
+        for i in graph[k]:
+            if visited[i] == 0:
+                queue.append(i)
+
+for i in range(1, n+1):
+    print(visited[i])
