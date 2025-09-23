becd44c9697b0afee60e3e910c380a02f82a3e55
eunbin-hyun <eunbin8592@gmail.com>
2025-09-23 17:39:47 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,11 +3,9 @@ from collections import deque
 n,m = map(int, input().split())
 array = []
 
+#붙어있는 문자 나눠서 리스트 저장
 for i in range(n):
-    k = input()
-    array.append([])
-    for j in k:
-        array[i].append(int(j))
+    array.append(list(map(int,input())))
 
 q = deque()
 
@@ -28,8 +26,9 @@ def bfs(x,y):
             if nx < 0 or nx >= n or ny <0 or ny >=m:
                 continue
             elif array[nx][ny] == 1:
-                array[nx][ny] = array[x][y] +1
+                array[nx][ny] = array[x][y] +1 # 이전숫자에서 +1 업데이트
                 q.append((nx,ny))
-    return array[n-1][m-1]
+
+    return array[n-1][m-1] # 도착 리턴
     
 print(bfs(0,0))
