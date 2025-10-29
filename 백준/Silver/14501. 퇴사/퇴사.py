2c84b48a11976bd3ec875544587f427334b18c6e
eunbin-hyun <eunbin8592@gmail.com>
2025-10-29 17:47:51 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -4,10 +4,8 @@ for _ in range(n):
     sc.append(list(map(int, input().split())))
 
 dp = [0]*n
-if sc[0][0] <= n:
-    dp[0] = sc[0][1]
 
-for i in range(1,n):
+for i in range(n):
     if i + sc[i][0] <= n:
         dp[i] = max(dp[i], sc[i][1])
         for j in range(0,i):
