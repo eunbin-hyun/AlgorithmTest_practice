3ebb8a4b88ff15549a4e5da2f51a80bbbe952fa4
eunbin-hyun <eunbin8592@gmail.com>
2025-11-15 11:27:49 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.76 ms, Memory: 9.31 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,30 @@
+def solution(survey, choices):
+    answer = ''
+    dic = {'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0}
+    # 성격 지표 판단
+    for i in range(len(survey)):
+        survey[i] = list(survey[i])
+        if choices[i] < 4:
+            dic[survey[i][0]] += 4-choices[i]
+        elif choices[i] > 4:
+            dic[survey[i][1]] += choices[i] - 4
+            
+    # 성격 지표 결과
+    if dic['R'] >= dic['T']:
+        answer += 'R'
+    else:
+        answer += 'T'
+    if dic['C'] >= dic['F']:
+        answer += 'C'
+    else:
+        answer += 'F'
+    if dic['J'] >= dic['M']:
+        answer += 'J'
+    else:
+        answer += 'M'
+    if dic['A'] >= dic['N']:
+        answer += 'A'
+    else:
+        answer += 'N'
+
+    return answer
